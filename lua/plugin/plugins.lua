e8b46743c0f4b1dc55f50671f0bd1d805fadce61
ZhongsJie <zhongsjie@gmail.com>
2022-12-02 00:24:10 +0800

    Feat: add treesitter configuration

--- lua/plugin/plugins.lua
+++ lua/plugin/plugins.lua
@@ -78,8 +78,11 @@ return packer.startup(function(use)
     "nvim-treesitter/nvim-treesitter",
     run = ":TSUpdate",
   }
-
   use "p00f/nvim-ts-rainbow"
+
+  -- Autopairs
+
+  use "windwp/nvim-autopairs"
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
--- lua/plugin/treesitter.lua
+++ lua/plugin/treesitter.lua
@@ -5,7 +5,7 @@ end
 
 configs.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "json", "lua", "java", "yaml", "markdown", "lua" },
+  ensure_installed = { "bash", "c", "go", "python", "json", "lua", "java", "yaml", "markdown", "lua" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
